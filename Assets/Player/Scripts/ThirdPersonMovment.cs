00ae99fa30abb5374becbab59f2148558bd3985a
Author <zilow1234@gmail.com>
2025-08-05 00:17:43 +0200

    Add player statistics and use it in movement

--- Assets/Player/Scripts/ThirdPersonMovment.cs
+++ Assets/Player/Scripts/ThirdPersonMovment.cs
@@ -1,78 +1,74 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
-namespace Player {
-[RequireComponent(typeof(CharacterController))]
-public class ThirdPersonMovment : MonoBehaviour
+using Player.Statistics;
+
+namespace Player
 {
-    public float walkSpeed = 3f;
-    public float runSpeed = 6f;
-    public float rotationSpeed = 10f;
-
-    private CharacterController controller;
-    private Transform cameraTransform;
-
-    private Vector2 inputMovement = Vector2.zero;
-    private bool isRunPressed = false;
-
-    void Start()
+    [RequireComponent(typeof(CharacterController))]
+    public class ThirdPersonMovment : MonoBehaviour
     {
-        controller = GetComponent<CharacterController>();
-        cameraTransform = Camera.main.transform;
-    }
+        public float rotationSpeed = 10f;
 
-    public void OnMove(InputValue value)
-    {
-        inputMovement = value.Get<Vector2>();
-    }
+        private CharacterController controller;
+        private Transform cameraTransform;
 
-    public void OnRun(InputValue value)
-    {
-        Debug.Log("OnRun");
-        isRunPressed = value.isPressed;
-    }
+        private Vector2 inputMovement = Vector2.zero;
+        private bool isRunPressed = false;
 
-    void Update()
-    {
-        // Kierunki kamery
-        Vector3 camForward = cameraTransform.forward;
-        Vector3 camRight = cameraTransform.right;
-        camForward.y = 0f;
-        camRight.y = 0f;
-        camForward.Normalize();
-        camRight.Normalize();
+        [SerializeField] private Statistics.Statistics stats;
 
-        // Kierunek ruchu względem kamery
-        Vector3 moveDir = camForward * inputMovement.y + camRight * inputMovement.x;
-        moveDir.Normalize();
-
-        float forwardDot = Vector3.Dot(camForward, moveDir);
-        bool isMovingBackward = forwardDot < -0.5f;
-        bool isMoving = moveDir.sqrMagnitude > 0.01f;
-
-        // Bieganie: tylko do przodu + Shift
-        bool isRunning = isMoving && !isMovingBackward && isRunPressed;
-        float currentSpeed = isRunning ? runSpeed : walkSpeed;
-
-        // Obrót
-        if (isMoving)
+        void Start()
         {
-            Vector3 facingDir = isMovingBackward ? -moveDir : moveDir;
-            Quaternion targetRotation = Quaternion.LookRotation(facingDir, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            controller = GetComponent<CharacterController>();
+            cameraTransform = Camera.main.transform;
         }
 
-        // Ruch
-        controller.SimpleMove(moveDir * currentSpeed);
-
-        // Blend animacji (MoveY: -1 = back, 0 = idle, 1 = walk, 2 = run)
-        float moveY = 0f;
-        if (isMoving)
+        public void OnMove(InputValue value)
         {
-            if (isMovingBackward)
-                moveY = -1f;
-            else
-                moveY = isRunning ? 2f : 1f;
+            inputMovement = value.Get<Vector2>();
+        }
+
+        public void OnRun(InputValue value)
+        {
+            isRunPressed = value.isPressed;
+        }
+
+        void Update()
+        {
+            Vector3 camForward = cameraTransform.forward;
+            Vector3 camRight = cameraTransform.right;
+            camForward.y = 0f;
+            camRight.y = 0f;
+            camForward.Normalize();
+            camRight.Normalize();
+
+            Vector3 moveDir = camForward * inputMovement.y + camRight * inputMovement.x;
+            moveDir.Normalize();
+
+            float forwardDot = Vector3.Dot(camForward, moveDir);
+            bool isMovingBackward = forwardDot < -0.5f;
+            bool isMoving = moveDir.sqrMagnitude > 0.01f;
+
+            bool isRunning = isMoving && !isMovingBackward && isRunPressed;
+            float currentSpeed = isRunning ? stats.RunSpeed : stats.WalkSpeed;
+
+            if (isMoving)
+            {
+                Vector3 facingDir = isMovingBackward ? -moveDir : moveDir;
+                Quaternion targetRotation = Quaternion.LookRotation(facingDir, Vector3.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+
+            controller.SimpleMove(moveDir * currentSpeed);
+
+            float moveY = 0f;
+            if (isMoving)
+            {
+                if (isMovingBackward)
+                    moveY = -1f;
+                else
+                    moveY = isRunning ? 2f : 1f;
+            }
         }
     }
-}
 }
