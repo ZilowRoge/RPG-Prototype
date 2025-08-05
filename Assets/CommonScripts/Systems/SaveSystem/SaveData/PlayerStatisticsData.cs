84c526a43ace3c643473cbffb75ad66fb9e3c348
Author <zilow1234@gmail.com>
2025-08-05 18:54:37 +0200

    Add system for saving game

---
+++ Assets/CommonScripts/Systems/SaveSystem/SaveData/PlayerStatisticsData.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Systems.SaveSystem.SaveData {
+    [Serializable]
+    public class PlayerStatisticsData {
+        public float currentHealth;
+        public float currentMana;
+        public float currentStamina;
+    }
+}
